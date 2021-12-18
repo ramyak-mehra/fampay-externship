66f35c201e3751e5e7d7f511477b339e0622b8ae
Ramyak Mehra <rmehra_be19@thapar.edu>
2021-12-18 19:57:08 +0530

    Added channel name field

--- videodata/models.py
+++ videodata/models.py
@@ -3,7 +3,21 @@ from django.db import models
 # Create your models here.
 class VideoDataModel(models.Model):
     video_id = models.CharField(max_length=100)
-    # youtube video title max length is 100
+    # youtube video title max length is 120. 20 more just in case it chanegs.
     title = models.CharField(max_length=100)
     description = models.TextField()
     published = models.DateTimeField()
+    # youtube channel name max length is 60. 100 just in case the it changes.
+    channel_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.title}_{self.video_id}"
+class Thumbnail(models.Model):
+    video_id = models.ForeignKey(VideoDataModel, on_delete=models.CASCADE)
+    tite = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='thumbnails')
+    width = models.IntegerField()
+    height = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.title}_{self.video_id}"
