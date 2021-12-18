00862994b6a127397d824018f4ca7e8677427978
Ramyak Mehra <rmehra_be19@thapar.edu>
2021-12-19 01:41:48 +0530

    searching and sorting on videodatamodel

--- videodata/views.py
+++ videodata/views.py
@@ -1,8 +1,9 @@
+from django_filters.rest_framework import filters
 from .serializer import VideoDataSerializer
 from .models import VideoDataModel
 from .pagination import VideoDataCursorPagination
 from rest_framework import pagination, viewsets
-
+from rest_framework import filters
 """
 We are using viewsets.ReadOnlyModelViewSet 
 because we only need to read data from the database.
@@ -15,4 +16,9 @@ class VideoDataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = VideoDataModel.objects.all()
     serializer_class = VideoDataSerializer
     pagination_class  = VideoDataCursorPagination
+    filter_backends = [filters.SearchFilter , filters.OrderingFilter]
+    search_fields= ['title' , 'description' , 'video_id']
+    ordering_fields = ['published' , 'title' , 'channel_name']
+    ordering = ['-published']
+
 
