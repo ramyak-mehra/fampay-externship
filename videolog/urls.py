d7cecfaa6162382ff6de48de63a57079746795d5
Ramyak Mehra <rmehra_be19@thapar.edu>
2021-12-19 17:08:48 +0530

    media folder access in debug mode

--- videolog/urls.py
+++ videolog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path , include
 from videodata import views as vdataviews
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'videodata', vdataviews.VideoDataViewSet)
@@ -25,3 +27,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
