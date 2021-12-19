f17909e60c8534e71f1b695ce896155574db088e
Ramyak Mehra <rmehra_be19@thapar.edu>
2021-12-19 17:01:04 +0530

    caching for the videodata get requests

--- videodata/views.py
+++ videodata/views.py
@@ -4,6 +4,9 @@ from .models import VideoDataModel
 from .pagination import VideoDataCursorPagination
 from rest_framework import pagination, viewsets
 from rest_framework import filters
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+
 """
 We are using viewsets.ReadOnlyModelViewSet 
 because we only need to read data from the database.
@@ -20,5 +23,15 @@ class VideoDataViewSet(viewsets.ReadOnlyModelViewSet):
     search_fields= ['title' , 'description' , 'video_id']
     ordering_fields = ['published' , 'title' , 'channel_name']
     ordering = ['-published']
-
-
+    """
+    We cache the result for 10 minutes. As new data is only available only after 10 mins.
+    We are currently using the default local memory caching. But can be easily replaced with redis
+    or any other by configuring the CACHE in the settings file. No changes are required here.
+    """
+    @method_decorator(cache_page(60*10))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @method_decorator(cache_page(60*10))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
